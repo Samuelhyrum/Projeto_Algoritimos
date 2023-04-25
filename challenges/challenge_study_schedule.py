219ba8cdbabde99674b0fc020020d486efc46c7f
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-25 17:18:56 -0300

    Requisito 1, Número de estudantes estudando no mesmo horário (Algoritmo de busca), força bruta

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    try:
+        max_count = sum(
+            1 for entry, exit in permanence_period
+            if entry <= target_time <= exit
+        )
+
+        return max_count
+    except TypeError:
+        return None
