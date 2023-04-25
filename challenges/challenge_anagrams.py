47868be1e74ae2aac454ba01f357a0739d0f1115
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-25 19:53:43 -0300

    Requisito 4, Anagramas (Algoritmo de ordenação)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,16 @@
+def counting_sort(s):
+    count = [0] * 26
+    for c in s:
+        count[ord(c.lower()) - ord('a')] += 1
+    res = ""
+    for i in range(26):
+        res += chr(ord('a') + i) * count[i]
+    return res
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if first_string == '' and second_string == '':
+        return (first_string, second_string, False)
+    word_1 = counting_sort(first_string)
+    word_2 = counting_sort(second_string)
+    return (word_1, word_2, word_1 == word_2)
+
