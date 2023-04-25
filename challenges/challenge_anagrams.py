d8ffa9a989f2adb0333dae3bbb6141c4fe658137
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-25 19:58:36 -0300

    Linter

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -7,10 +7,10 @@ def counting_sort(s):
         res += chr(ord('a') + i) * count[i]
     return res
 
+
 def is_anagram(first_string, second_string):
     if first_string == '' and second_string == '':
         return (first_string, second_string, False)
     word_1 = counting_sort(first_string)
     word_2 = counting_sort(second_string)
     return (word_1, word_2, word_1 == word_2)
-
