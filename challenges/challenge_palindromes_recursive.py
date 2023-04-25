9ba5b1ff1b6db3a4621b03cbb5d12921ee955a75
Samuel Hyrum <samuelhymp@gmail.com>
2023-04-25 18:50:39 -0300

    Requisito 3, Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    elif len(word) == 1:
+        return True
+    elif len(word) == 2:
+        return word[0] == word[1]
+    elif word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word[1:-1], 0, - 1)
